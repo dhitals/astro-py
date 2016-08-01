076aaf412dbc77227ea6382307b39466df0c38da
saurav <dhitals@me.com>
2016-07-31 20:36:29 -0400

    Added README.

--- slowpokes.sql
+++ slowpokes.sql
@@ -1,33 +1,22 @@
--- query to search around cataloged stars within 30" in the SDSS STAR database
--- assumes a output table has already been created with a separate query.
+-- Examples of a query used in the original slowpokes work.
+-- queries the PhotoObjAll and the ProperMotions table in SDSS
 
--- Clean the photometry. This can be omitted and done later in IDL for speed.
-@PEAKCENTER=dbo.fPhotoFlags('PEAKCENTER')declare @NOTCHECKED bigint set @NOTCHECKED=dbo.fPhotoFlags('NOTCHECKED')
+CREATE TABLE #upload (up_ra FLOAT, up_dec FLOAT, up_id BIGINT );
+INSERT INTO #upload SELECT TOP 2 ra as up_ra, dec as up_dec, objID as up_id FROM MYDB.BooHighPM
+CREATE TABLE #tmp (up_id BIGINT, objID BIGINT);
+INSERT INTO #tmp EXEC spGetNeighbors 1
 
-declare @PSF_FLUX_INTERP bigint set @PSF_FLUX_INTERP=dbo.fPhotoFlags('PSF_FLUX_INTERP')
-declare @INTERP_CENTER bigint set @INTERP_CENTER=dbo.fPhotoFlags('INTERP_CENTER')
-declare @BAD_COUNTS_ERROR bigint set @BAD_COUNTS_ERROR=dbo.fPhotoFlags('BAD_COUNTS_ERROR')
-declare @SATURATED bigint set @SATURATED=dbo.fPhotoFlags('SATURATED')
-declare @BRIGHT bigint set @BRIGHT=dbo.fPhotoFlags('BRIGHT')
-declare @NODEBLEND bigint set @NODEBLEND=dbo.fPhotoFlags('NODEBLEND')
-declare @DEBLEND_NOPEAK bigint set @DEBLEND_NOPEAK=dbo.fPhotoFlags('DEBLEND_NOPEAK')
+INSERT INTO MYDB.HighPMSDSS SELECT
+t.up_id as target_id, t.objID,
+S.ra, S.dec, pm.pmra, pm.pmdec, pm.pmraerr, pm.pmdecerr,
+S.run, S.rerun, S.camcol, S.field, S.obj,
+S.psfMag_u, S.psfMag_g, S.psfMag_r, S.psfMag_i, S.psfMag_z,
+S.psfMagErr_u, S.psfMagErr_g, S.psfMagErr_r, S.psfMagErr_i, S.psfMagErr_z,
+S.extinction_u, S.extinction_g, S.extinction_r, S.extinction_i, S.extinction_z,
+S.flags, S.status, S.type, S.SpecObjID,
+pm.pmL, pm.pmB, pm.delta, pm.match, pm.nFit, pm.dist22,
+pm.sigRa, pm.sigDec, pm.O as usnoO, pm.J as usnoJ
 
-declare @bad_flags bigint set @bad_flags=(@PEAKCENTER|@NOTCHECKED|@PSF_FLUX_INTERP|@INTERP_CENTER|@BAD_COUNTS_ERROR|@SATURATED|@BRIGHT|@NODEBLEND|@DEBLEND_NOPEAK)
-
-SELECT
-  N.objID as targetID, N.NeighborObjID as objID,
-  S.ra, S.dec,
-  S.psfmag_u,S.psfmag_g,S.psfmag_r,S.psfmag_i,S.psfmag_z,
-  S.psfmagerr_u,S.psfmagerr_g,S.psfmagerr_r,S.psfmagerr_i,S.psfmagerr_z,
-  S.extinction_r
-  INTO MYDB.N01
-FROM MYDB.L01 as L, Neighbors as N JOIN STAR as S on S.objID = N.NeighborObjID
-WHERE N.objID = L.objID
-  and N.mode = 1 and N.neighborMode = 1
-  and N.type = 6 and N.neighborType = 6
-  and (S.flags_r & @bad_flags) = 0 and (S.flags_i & @bad_flags) = 0 and (S.flags_z & @bad_flags) = 0
-  and S.nChild = 0
-  and S.psfmagerr_r > 0 and S.psfmagerr_i > 0 and S.psfmagerr_z > 0
-  and abs(S.psfmagerr_i) <= 0.10 and abs(S.psfmagerr_z) <= 0.10
-  and S.extinction_r <= 0.50
-ORDER BY N.objID
+FROM #tmp t, dr6.PhotoObjAll S, dr6.ProperMotions pm, MYDB.BooHighPM Boo
+WHERE t.up_id = S.objID and t.up_id = pm.objID and S.objID = pm.objID
+ORDER BY Boo.up_id
